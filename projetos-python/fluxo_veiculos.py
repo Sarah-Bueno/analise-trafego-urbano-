22ada87c49251cf15e991f25708fb30b7eecf776
Sarah Bueno <sarahbrjakubowski@gmail.com>
2026-06-16 11:41:45 -0300

    Update fluxo_veiculos.py

--- projetos-python/fluxo_veiculos.py
+++ projetos-python/fluxo_veiculos.py
@@ -1,1 +1,33 @@
+"""
+Faça um programa em Python para cadastrar os veículos de uma locadora de
+automóveis. Inicialmente, o programa deve solicitar o cadastro dos veículos e,
+ao final, exibir todos os veículos cadastrados. Para cada veículo, representado
+por um dicionário, devem ser cadastradas as seguintes informações:
+▪ Placa;
+▪ Marca;
+▪ Modelo;
+▪ Cor.
+Quando o usuário informar “0” como placa, o cadastro deve ser encerrado e
+os veículos devem ser listados.
 
+"""
+placa = input("informe a placa: ")
+
+A = []
+
+while placa != "0":
+    Marca = input("informe a marca: ")
+    Modelo = input("informe a modelo: ")
+    Cor = input("informe a cor: ")
+
+    locadora = {
+        "Placa": placa,
+        "Marca": Marca,
+        "Modelo": Modelo,
+        "Cor": Cor,
+    }
+    A.append(locadora)
+    placa = input("informe a placa: ")
+
+for veiculo in A:
+    print(veiculo)
